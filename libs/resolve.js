577a8ad7360c7d28ac05a2124e6b3a1e9e9cfa54
nanocloudx <nanocloudx@gmail.com>
2015-11-28 19:22:09 +0900

    update resolve

--- libs/resolve.js
+++ libs/resolve.js
@@ -2,11 +2,11 @@ var response = require('./response')
 var status = require('./status');
 
 module.exports = function Resolve(data) {
-  if (!data) {
+  if (data === undefined) {
     return Promise.resolve(response(status.CONTINUE));
   }
   if (data.length === 0) {
-    return Promise.resolve(response(status.SUCCESS_NO_CONTENT, null));
+    return Promise.resolve(response(status.SUCCESS_NO_CONTENT, data));
   }
   return Promise.resolve(response(status.SUCCESS, data));
 };
