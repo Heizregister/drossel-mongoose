9c8d9226d011e8787fee7fd8031720abac0524d3
nanocloudx <nanocloudx@gmail.com>
2015-11-28 19:55:38 +0900

    bugfix

--- libs/resolve.js
+++ libs/resolve.js
@@ -5,7 +5,7 @@ module.exports = function Resolve(data) {
   if (data === undefined) {
     return Promise.resolve(response(status.CONTINUE));
   }
-  if (data.length === 0) {
+  if (data && data.length === 0) {
     return Promise.resolve(response(status.SUCCESS_NO_CONTENT, data));
   }
   return Promise.resolve(response(status.SUCCESS, data));
